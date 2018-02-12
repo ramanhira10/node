07c013f58a56fb0fce018d4c7719024cd965d4cb
raman <ramanhira@gmail.com>
2018-02-12 22:05:52 +0100

    adding items via ajax

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,13 +1,17 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+
 module.exports = function (app) {
 
   app.get('/todo', function (req, res) {
-    res.render('todo');
-
-
+    res.render('todo', {todos: data});
   });
 
-  app.post('/todo', function (req, res) {
-
+  app.post('/todo', urlencodedParser, function (req, res) {
+    data.push(req.body);
+    res.json(data);
   });
 
   app.delete('/todo', function (req, res) {
